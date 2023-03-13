a875611f6c021acdd8da8f97616034d3d69742ee
Justin Kurdila <justinkurdila@gmail.com>
2023-03-12 20:51:43 -0400

    adding hours to activities list

--- src/components/ActivityItem.js
+++ src/components/ActivityItem.js
@@ -14,7 +14,7 @@ const ActivityItem = (props) => {
 
 	return (
 		<li className="list-group-item d-flex justify-content-between align-items-center">
-			{props.name}
+			{props.name} | {props.hour} Hours
 			<div>
 				<span className="badge badge-primary badge-pill mr-3">{props.hour}</span>
 				<TiDelete size="1.5em" onClick={handleDeleteActivity}></TiDelete>
